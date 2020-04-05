8cba3ce26074f5b9f825cedc4063500ff7d5d859
Ciaran Doherty <cad106uk@gmail.com>
2020-04-05 13:29:46 +0100

    Basic JSON read

--- main.go
+++ main.go
@@ -1,24 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 )
+
+type JokeJSON struct {
+	ID        int    `json:"id"`
+	Type      string `json:"type"`
+	Setup     string `json:"setup"`
+	Punchline string `json:"punchline"`
+}
+
+var knownJokesCache = make(map[int]JokeJSON)
 
 func main() {
 	response, err := http.Get("https://official-joke-api.appspot.com/jokes/programming/ten")
 
 	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		log.Fatal(err.Error())
 	}
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(responseData))
+
+	var rawRead []JokeJSON
+	err = json.Unmarshal(responseData, &rawRead)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, val := range rawRead {
+		knownJokesCache[val.ID] = val
+	}
+	fmt.Printf("%+v", knownJokesCache)
 }
